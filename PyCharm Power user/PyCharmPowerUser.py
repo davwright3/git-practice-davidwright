57c7135ddc116c22145b0e441a4e54cd4052a767
davwright3 <dwright3@ualr.edu>
2025-02-18 13:12:31 -0600

    Rename update 3
    Renamed the rest of the variables, "x_coord", "y_coord".
    
    Renamed functions to make them more clear.
    
    Rewrote some of the player instructions and the text written to console to help the player understand how to interact with the board better.

--- PyCharm Power user/PyCharmPowerUser.py
+++ PyCharm Power user/PyCharmPowerUser.py
@@ -1,19 +1,19 @@
 def show_gameboard(b):
- for r in b:
-  print(" | ".join(r))
-  print("-"*5)
+  for r in b:
+   print(" | ".join(r))
+   print("-"*5)
  
  
-def c_w(b,p):
- for i in range(3):
-  if all(b[i][j]==p for j in range(3)) or all(b[j][i]==p for j in range(3)):
+def game_won(b, p):
+  for i in range(3):
+   if all(b[i][j]==p for j in range(3)) or all(b[j][i]==p for j in range(3)):
+    return True
+  if all(b[i][i]==p for i in range(3)) or all(b[i][2-i]==p for i in range(3)):
    return True
- if all(b[i][i]==p for i in range(3)) or all(b[i][2-i]==p for i in range(3)):
-  return True
- return False
+  return False
  
  
-def f(b):
+def board_full(b):
  return all(c!=" " for r in b for c in r)
  
  
@@ -26,19 +26,19 @@ def app():
    current_player=players[turn%2]
    while 1:
     try:
-     r,c=map(int,input(f"P {current_player}, row col (0-2): ").split())
-     if gameboard[r][c]==" ":
-      gameboard[r][c]=current_player
+     x_coord,y_coord=map(int,input(f"Player {current_player}, enter row col (between 0-2): ").split())
+     if gameboard[x_coord][y_coord]==" ":
+      gameboard[x_coord][y_coord]=current_player
       break
      else:
-      print("Nope. Again.")
+      print("Nope, that space is taken. Try again.")
     except:
-     print("Wrong. 0-2 pls.")
+     print("Invalid grid, enter integer between 0-2 pls.")
    show_gameboard(gameboard)
-   if c_w(gameboard,current_player):
+   if game_won(gameboard, current_player):
     print(f"P {current_player} wins!")
     return
-   if f(gameboard):
+   if board_full(gameboard):
     print("Draw!")
     return
   print("Draw!")
