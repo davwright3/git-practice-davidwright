f5803ca45f5dc4e08f3cd1079dcb5b008a1d0159
davwright3 <dwright3@ualr.edu>
2025-02-18 12:59:04 -0600

    Refactor Names 1
    Renamed the main app function to :app", and renamed the display board function to "display_gameboard"

--- PyCharm Power user/PyCharmPowerUser.py
+++ PyCharm Power user/PyCharmPowerUser.py
@@ -1,4 +1,4 @@
-def p(b):
+def show_gameboard(b):
  for r in b:
   print(" | ".join(r))
   print("-"*5)
@@ -17,31 +17,31 @@ def f(b):
  return all(c!=" " for r in b for c in r)
  
  
-def t():
- b=[[" "for _ in range(3)]for _ in range(3)]
- p=["X","O"]
- print("Tic-Tac-Toe Game")
- p(b)
- for t in range(9):
-  pl=p[t%2]
-  while 1:
-   try:
-    r,c=map(int,input(f"P {pl}, row col (0-2): ").split())
-    if b[r][c]==" ":
-     b[r][c]=pl
-     break
-    else:
-     print("Nope. Again.")
-   except:
-    print("Wrong. 0-2 pls.")
-  p(b)
-  if c_w(b,pl):
-   print(f"P {pl} wins!")
-   return
-  if f(b):
-   print("Draw!")
-   return
- print("Draw!")
+def app():
+  b=[[" "for _ in range(3)]for _ in range(3)]
+  p=["X","O"]
+  print("Tic-Tac-Toe Game")
+  show_gameboard(b)
+  for t in range(9):
+   pl=p[t%2]
+   while 1:
+    try:
+     r,c=map(int,input(f"P {pl}, row col (0-2): ").split())
+     if b[r][c]==" ":
+      b[r][c]=pl
+      break
+     else:
+      print("Nope. Again.")
+    except:
+     print("Wrong. 0-2 pls.")
+   show_gameboard(b)
+   if c_w(b,pl):
+    print(f"P {pl} wins!")
+    return
+   if f(b):
+    print("Draw!")
+    return
+  print("Draw!")
  
  
-t() 
+app()
